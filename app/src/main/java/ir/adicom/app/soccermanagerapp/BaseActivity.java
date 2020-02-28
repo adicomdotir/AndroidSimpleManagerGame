6d119b9982812636364cf7af6bc7bc872b6da9f3
adicomdotir <adicom.ir@gmail.com>
2020-02-28 23:40:22 +0330

    WP: Add bottom navigator to game section

--- app/src/main/java/ir/adicom/app/soccermanagerapp/BaseActivity.java
+++ app/src/main/java/ir/adicom/app/soccermanagerapp/BaseActivity.java
@@ -19,17 +19,18 @@ public class BaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_base);
 
         ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionBar.setCustomView(R.layout.actionbar_layout);
 
-        Button btnNew = (Button) findViewById(R.id.btn_new);
+        Button btnNew = findViewById(R.id.btn_new);
         btnNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
-        Button btnLoad = (Button) findViewById(R.id.btn_load);
+        Button btnLoad = findViewById(R.id.btn_load);
         btnLoad.setEnabled(false);
     }
 
